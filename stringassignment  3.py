00d537746320a2a3aa2e8490f7dbc4c58ee38c04
Mohammad Sajid <159249332+mdsajid2003@users.noreply.github.com>
2024-09-20 03:58:19 +0530

    Add files via upload
    Everyone working with PDFs must have seen the CTRL+F option, where you can search
    for a word and the output says how many such words are there in the PDF document. It
    also offers the option of replacement.
    So, write a function that takes a ".txt" file, a string, and another keyword argument
    "operation" as input and does the following:
    • If the operation is given as "find". Then it says the number of times that the input
    string has been present in the ".txt file".
    • If the operation is given as "replace," then it needs another keyword argument=,
    "serial" and "replacement." If the serial is "i," then the i-th instance of the original
    word is replaced by the "replacement" string. The serial must not be greater than
    the number of instances of the original string that has been repeated in the ".txt"
    file. Otherwise, the function should output an error.
    • If the operation is given as "replace-all" then the keyword serial is not needed,
    but the "replacement" keyword is needed. All the original instances of the word is
    replaced by the replacement in the file.

---
+++ stringassignment  3.py
@@ -0,0 +1,41 @@
+#Showing the options
+print('Enter the following options\n'
+      'For finding:              "find"\n'
+      'For replacing with serial "replace"\n'
+      'For replacing all :       "replace all"')
+#Taking the input from the user
+choice = input('Enter your choice: ')
+#opening the file and reading it
+with open('example.txt','r') as f:
+    data = f.read()
+c = 0
+#checking the choice of the user
+if choice == 'find':   #the user choosing find
+    words = data.split()
+    len_words = len(words)
+    print(data)
+    inpst = input('Enter the string you want to find: ')
+    for _ in words:
+        if _ == inpst:
+            c +=1
+    print(f'Word "{inpst}": {c} ')  
+elif choice == 'replace':   #user choosing replace
+    words = data.split()
+    len_words = len(words)
+    print(data)
+    print(f'words: {len_words}')
+    replace = input('Enter the string you want to replace ')   #taking the string to be replace
+    replacing =  input('Enter the string  to replace ')        #taking the new string to be placed
+    serial  = int(input(f'enter the serial number to be placed replaced '
+                        'there are {len_words} in the string'))
+    if serial != len_words:
+        data = data.replace(replace,replacing,serial)
+        print(data)
+    else:
+        print('error')
+elif choice == 'replace all':  #user choosing replace all
+    print(data)
+    toreplace = input('Enter the string you want to replace ')  #taking the string to be replace
+    replaced =  input('Enter the string  to replace ')          #taking the new string to be placed
+    data = data.replace(toreplace,replaced)
+    print(data)
